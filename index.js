a23908a770a128f3a5d747a7d1b678077e3c777a
Yevhen Surfer <23skopets@gmail.com>
2024-05-21 09:55:35 +0200

    for loop cards

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+// Create a function, getRandomCard(), that always returns the number 5
+
+
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -32,7 +39,6 @@ function renderGame() {
 function newCard() {
     let card = 6
     sum += card
-    // Push the card to the cards array
     cards.push(card)
     console.log(cards)
     renderGame()
